16e028822b0b680e05dcae8257b3775f51443396
teamzz111 <felipeflogxd@gmail.com>
2019-01-22 12:12:07 -0500

    First step completed

--- js/index.js
+++ js/index.js
@@ -1,6 +1,20 @@
 $(document).ready(function(){
+    $('section.step2, section.step3').fadeOut(1);
     $("#fileToUpload").submit(function(e){
-        alert("quieto");
-        e.preventDefault();
+        $('section.step1').fadeOut(1000, function(){
+            $('section.step2').fadeIn(1000, function(){
+                
+            })
+        })
+        e.preventDefault()
     })
+    $("input:file").change(function (){
+        var fileName = $(this).val()
+        $('.step1 .container .der form i').removeClass()
+        $('.step1 .container .der form i').addClass("fa")
+        $('.step1 .container .der form i').addClass("fa-check")
+        $('.step1 .container .der form span').text("Archivo cargado con éxito")
+        $('.step1 .container .der form .label').css({border: "2px dashed lightgreen"})
+        $('.step1 .container .der form .title').css("color", "black")
+    });
 })
